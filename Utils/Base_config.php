ee7e369fb669db351bd3288dfefec294d15c7c4b
Swizz540 <quentin.g@laposte.net>
2014-01-05 18:19:02 +0100

    Create basics configurations

---
+++ Utils/Base_config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    Les configurations présentées ici sont utile au bon fonctionement de la solution.
+    Vous devez les modifiers en connaissance de cause et à vos risques et périles.
+*/
+
+ /* * * * * * * * * * * * * * * *\
+|      Configuration de base      |
+ \* * * * * * * * * * * * * * * */
+
+// La solution est en mode DEBUG
+ define('debug', true);
+
+
+
+ /* * * * * * * * * * * * * * * *\
+|    Chargement des dépendances   |
+ \* * * * * * * * * * * * * * * */
+
+// Appel des loaders des dépendances
+ require_once 'Dependencies/autoload.php';
+
+
+
